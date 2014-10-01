67f88b6c7f42ac1912d19109e7e27df431c24dcd
Seth Johnson <seth.adams.johnson@gmail.com>
2014-10-01 10:04:41 -0400

    Ran hydra generator

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '66a356e705c482cc421473f907fe1fdbee6e7b7333c409cb6d2ef726f8b831314c873b61e1ccf9a8e6c12c08a3967e686ba386ad9c11366be69e35ac5f089613'
+
